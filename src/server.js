a9dff275a48195f2b038301a554ba911641bf3af
김민수 <kokbee@kokbee.local>
2021-06-10 22:37:32 +0900

    test middleware

--- src/server.js
+++ src/server.js
@@ -3,16 +3,20 @@ import express from "express";
 const PORT = 8005;
 const app = express();
 
-const handleHome = (req, res) =>{
-    return res.send("home");
-}
+// middle
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    console.log(`${req.path}`);
+    // 다음으로 넘어감
+    next();
+  };
 
-const handleLogin = (req, res) =>{
-    return res.send("login");
-}
+// final
+const handleHome = (req, res) => {
+    return res.send("middlewares");
+};
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", logger, handleHome);
 
-const handleListening = () => console.log(`Run server on port ${PORT}`);
+const handleListening = () => console.log(`Run server on port:${PORT}`);
 app.listen(PORT, handleListening);
