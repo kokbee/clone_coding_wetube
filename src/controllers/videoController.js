53becfa57fb440850e7e0e92f1d02333e9496942
김민수 <kokbee@kokbee.local>
2021-06-12 23:24:35 +0900

    compare main

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,31 @@
 export const trending = (req, res) => {
     const videos = [
         {
-          title: "Hello",
-        },
-        {
-          title: "Video #2",
-        },
-        {
-          title: "Whatsup",
-        },
-      ];
-      return res.render("home", { pageTitle: "Home", videos });
+          title: "First Video",
+          rating: 5,
+          comments: 2,
+          createdAt: "2 minutes ago",
+          views: 59,
+          id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+      },
+      {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+      },
+    ];
+    return res.render("home", {pageTitle:"Home", videos});
 }
 export const see = (req, res) => {
     return res.render("watch",{pageTitle:"Watch"});
