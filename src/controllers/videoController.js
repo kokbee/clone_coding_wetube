d2f7000736fd8939a435695ea88c646c3c4f5de5
김민수 <kokbee@kokbee.local>
2021-06-12 23:07:41 +0900

    beta Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,31 @@
 export const trending = (req, res) => {
-    return res.render("home", {pageTitle:"Home"});
+    const videos = [
+        {
+          title: "First Video",
+          rating: 5,
+          comments: 2,
+          createdAt: "2 minutes ago",
+          views: 59,
+          id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+      },
+      {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+      },
+    ];
+    return res.render("home", {pageTitle:"Home", videos});
 }
 export const see = (req, res) => {
     return res.render("watch",{pageTitle:"Watch"});
