2bc646007706df1c97a4cb148b152f90272dda4d
kokbee <kok2209@gmail.com>
2021-06-16 21:42:47 +0900

    update login&session

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,6 +56,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wroon password",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import {localMiddleware} from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +14,15 @@ app.set("views", process.cwd() + "/src/views")
 app.use(logger);
 // express application이 from value를 이해하고 자바스크립트 형식으로 변경
 app.use(express.urlencoded({extended:true}))
+
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
