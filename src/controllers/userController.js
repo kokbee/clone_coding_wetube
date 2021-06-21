24ca673db9b4cf467456408f90b91a04860d31a1
kokbee <kok2209@gmail.com>
2021-06-21 10:58:11 +0900

    modify config

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -64,8 +64,8 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "Iv1.e5c36c7a70b4da1a",
-    allow_signup:false,
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
     scope:"read:user user:email",
   };
   const params = new URLSearchParams(config).toString();
@@ -73,7 +73,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Edit Remove");
